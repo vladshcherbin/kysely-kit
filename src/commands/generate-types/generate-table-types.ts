c6d4b3f32c535cd47a12bc219257f76e730529c2
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2024-03-24 23:24:21 +0300

    Generate types only for public schema

--- src/commands/generate-types/generate-table-types.ts
+++ src/commands/generate-types/generate-table-types.ts
@@ -4,37 +4,39 @@ import { StatementStructures, StructureKind } from 'ts-morph'
 import mapDataType from './map-data-type.js'
 
 export default function generateTableTypes(tables: TableMetadata[]): StatementStructures[] {
-  return tables.map((table) => ({
-    isExported: true,
-    kind: StructureKind.Interface,
-    name: camelcase(`${table.name}Table`, { pascalCase: true }),
-    properties: table.columns.map((column) => {
-      const name = camelcase(column.name)
-      const isArray = column.dataType.startsWith('_')
-      let type = mapDataType(isArray ? column.dataType.slice(1) : column.dataType)
+  return tables
+    .filter((table) => table.schema === 'public')
+    .map((table) => ({
+      isExported: true,
+      kind: StructureKind.Interface,
+      name: camelcase(`${table.name}Table`, { pascalCase: true }),
+      properties: table.columns.map((column) => {
+        const name = camelcase(column.name)
+        const isArray = column.dataType.startsWith('_')
+        let type = mapDataType(isArray ? column.dataType.slice(1) : column.dataType)
 
-      if (name === 'createdAt' && type === 'Date' && column.hasDefaultValue) {
-        type = 'GeneratedAlways<Date>'
-      } else if (name === 'updatedAt' && type === 'Date' && column.isNullable) {
-        type = 'ColumnType<Date | null, never, Date>'
-      } else {
-        if (isArray) {
-          type = `${type}[]`
+        if (name === 'createdAt' && type === 'Date' && column.hasDefaultValue) {
+          type = 'GeneratedAlways<Date>'
+        } else if (name === 'updatedAt' && type === 'Date' && column.isNullable) {
+          type = 'ColumnType<Date | null, never, Date>'
+        } else {
+          if (isArray) {
+            type = `${type}[]`
+          }
+
+          if (column.isAutoIncrementing) {
+            type = `GeneratedAlways<${type}>`
+          }
+
+          if (column.isNullable) {
+            type = `${type} | null`
+          }
         }
 
-        if (column.isAutoIncrementing) {
-          type = `GeneratedAlways<${type}>`
+        return {
+          name,
+          type
         }
-
-        if (column.isNullable) {
-          type = `${type} | null`
-        }
-      }
-
-      return {
-        name,
-        type
-      }
-    })
-  }))
+      })
+    }))
 }
