14a2eceb9737f95d5eadc6c637ca74df786846dc
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2026-05-12 01:36:43 +0300

    Remove js extension ending

--- src/utilities/generate-imports.ts
+++ src/utilities/generate-imports.ts
@@ -1,8 +1,5 @@
 import type { SourceFile } from 'ts-morph'
 
 export default function generateImports(sourceFile: SourceFile) {
-  sourceFile.fixMissingImports({}, {
-    importModuleSpecifierEnding: 'js',
-    preferTypeOnlyAutoImports: true
-  })
+  sourceFile.fixMissingImports({}, { preferTypeOnlyAutoImports: true })
 }
